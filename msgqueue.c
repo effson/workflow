a3de6edc814158c6210b7479b236924192fccfed
effson <113086278+effson@users.noreply.github.com>
2025-07-30 20:10:53 +0800

    Create msgqueue.c

---
+++ msgqueue.c
@@ -0,0 +1,21 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "msgqueue.h"
+
+struct __msgqueue
+{
+	size_t msg_max;  // 队列的最大消息数
+	size_t msg_cnt;  // 当前消息数
+	int linkoff;
+	int nonblock;
+	void *head1;
+	void *head2;
+	void **get_head;
+	void **put_head;
+	void **put_tail;
+	pthread_mutex_t get_mutex;
+	pthread_mutex_t put_mutex;
+	pthread_cond_t get_cond;
+	pthread_cond_t put_cond;
+};
